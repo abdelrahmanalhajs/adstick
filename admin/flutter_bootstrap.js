c2a3175b52713836bcea7e446681a2a2639c686b
abdelrahmanalhajs <abdelrahmanalhajs.com>
2026-06-14 22:35:10 +0300

    fix: settings ListView right padding to prevent FAB overlap on value fields

--- admin/flutter_bootstrap.js
+++ admin/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "178460908" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "52414397" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
